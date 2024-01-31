1436d01b2295a9f1ebabcaceafc42d2b838647e3
olivierhuynh-web <olivier.huynh@yahoo.fr>
2024-01-31 23:41:32 +0100

    version clean

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,7 @@ import './globals.css';
 import CustomFont from '@next/font/local';
 const tradeGothic = CustomFont({
   src: './fonts/Trade-Gothic-LT.woff',
-  variable: '--font-cfont',
 });
-
-// const cfont = CustomFont({
-//   src: '../public/fonts/cfont.ttf',
-//   variable: '--font-cfont',
-// })
 
 export default function RootLayout({ children }) {
   return (
@@ -18,5 +12,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
-// const ppNeue = localFont({ src: '../public/fonts/ppneueregular.woff2' })
